eb4a977fbca9a37add0e84b759c54a6fba5a3fab
Lulu Melhem <lulumelhem@ufl.edu>
2019-10-27 23:19:44 -0500

    First commit

--- Bootcamp3/server/config/express.js
+++ Bootcamp3/server/config/express.js
@@ -40,7 +40,7 @@ module.exports.init = function() {
      Sends a response (res) to go to the homepage for all routes not specified */ 
   app.all('/*', function(req, res) {
    
-  res.sendFile(path.resolve('client/index.html'));
+    res.sendFile(path.resolve('client/index.html'));
   });
   
   return app;
